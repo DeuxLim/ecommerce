9589e86d76f168fc18faa82d5f75681587c0dae0
ddlim <deux.lim@orro.group>
2024-09-09 19:33:02 +0800

    update product route

--- routes/web.php
+++ routes/web.php
@@ -17,16 +17,17 @@ use App\Http\Controllers\RegisterUserController;
 |
 */
 
-
-// product routes
+// Product Routes
 Route::get('/',[ProductController::class, 'index'])->name('product.index');
+Route::get('/product/{product}/show', [ProductController::class, 'show'])->name('product.show');
 Route::get('/product/create', [ProductController::class, 'create'])->name('product.create');
 Route::post('/product/create', [ProductController::class, 'store'])->name('product.store');
+Route::get('/product/{product}/edit', [ProductController::class, 'edit'])->name('product.edit');
+Route::patch('/product/{product}', [ProductController::class, 'update'])->name('product.update');
 
-// user routes
+// Auth and Session Routes
 Route::get('/register', [RegisterUserController::class, 'create'])->name('register');
 Route::post('/register', [RegisterUserController::class, 'store'])->name('register.store');
-
 route::post('/login', [SessionController::class, 'store']);
 route::get('/login', [SessionController::class, 'create'])->name('login');
 route::post('/logout', [SessionController::class, 'destroy'])->name('logout');
